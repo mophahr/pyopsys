175008008f41d81b51ef7a6eb0cbdc0b7e405f23
Moritz Schönwetter <msch@pks.mpg.de>
2015-07-23 10:25:47 +0200

    started fractal dimension module

---
+++ dimension_algorithms.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function
+"""
+    dimension_algorithms module
+
+    Copyright © 2012-2015 Moritz Schönwetter
+
+    This file is part of pyopsys.
+
+    pyopsys is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    pyopsys is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with pyopsys.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import numpy as np
+
+
+"""
+	box_counting algorithms with pre made sets:
+"""
+def __init__(self):
+    pass
+
+def box_counting_1D(filled_boxes,epsilons=np.logspace(-5,2,5),norm=1):
+
+	
